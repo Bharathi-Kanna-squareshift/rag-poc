f11517ddc1209211a22e65c9a668c9215e2b178e
Bharathi-Kanna <99189546+Bharathi-Kanna@users.noreply.github.com>
2024-05-08 23:26:06 +0530

    err change

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 # index.py
-import json, os
+import json
+import os
 from llama_index.core import Document, Settings
 from llama_index.core.node_parser import SentenceSplitter
 from llama_index.core.ingestion import IngestionPipeline
@@ -8,33 +9,28 @@ from llama_index.vector_stores.elasticsearch import ElasticsearchStore
 from dotenv import load_dotenv
 
 def get_documents_from_file(file):
-   """Reads a json file and returns list of Documents"""
-
-   with open(file=file, mode='rt') as f:
-       conversations_dict = json.loads(f.read())
+    """Reads a JSON file and returns a list of Documents"""
+    with open(file=file, mode='rt') as f:
+        conversations_dict = json.loads(f.read())
       
-   # Build Document objects using fields of interest.
-   documents = [Document(text=item['conversation'],
-                         metadata={"conversation_id": item['conversation_id']})
-                for
-                item in conversations_dict]
-   return documents
+    # Build Document objects using fields of interest.
+    documents = [Document(text=item['conversation'],
+                          metadata={"conversation_id": item['conversation_id']})
+                 for item in conversations_dict]
+    return documents
 
-
-# Load .env file contents into env
-# ELASTIC_CLOUD_ID and ELASTIC_API_KEY are expected to be in the .env file.
+# Load .env file contents into environment variables
 load_dotenv('.env')
 
-# ElasticsearchStore is a VectorStore that
-# takes care of ES Index and Data management.
+# Initialize ElasticsearchStore
 es_vector_store = ElasticsearchStore(index_name="calls",
                                      vector_field='conversation_vector',
                                      text_field='conversation',
                                      es_cloud_id=os.getenv("ELASTIC_CLOUD_ID"),
                                      es_api_key=os.getenv("ELASTIC_API_KEY"))
 
-
-def main():
+def run_ingestion_pipeline():
+    """Runs the ingestion pipeline to load data into Elasticsearch."""
     # Embedding Model to do local embedding using Ollama.
     ollama_embedding = OllamaEmbedding("mistral")
 
@@ -48,12 +44,12 @@ def main():
         vector_store=es_vector_store
     )
 
-    # Load data from a json file into a list of LlamaIndex Documents
+    # Load data from a JSON file into a list of LlamaIndex Documents
     documents = get_documents_from_file(file="conversations.json")
 
     pipeline.run(documents=documents)
     print(".....Done running pipeline.....\n")
 
-
+# Run the ingestion pipeline when executed as a script
 if __name__ == "__main__":
-    main()
+    run_ingestion_pipeline()
