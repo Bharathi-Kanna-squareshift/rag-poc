f412c609d52b9d98dadeb8d1d1a3790e5f43778b
Bharathi-Kanna <99189546+Bharathi-Kanna@users.noreply.github.com>
2024-05-08 23:00:53 +0530

    without mvc approch

--- main.py
+++ main.py
@@ -1,74 +1,13 @@
-import os
-import json
+# main.py
 from fastapi import FastAPI, HTTPException
-from llama_index.core import Document, Settings, IngestionPipeline
-from llama_index.core.node_parser import SentenceSplitter
-from llama_index.embeddings.ollama import OllamaEmbedding
-from llama_index.vector_stores.elasticsearch import ElasticsearchStore
-from dotenv import load_dotenv
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
 from query import run_query
 
 app = FastAPI()
 
-# Load .env file contents into environment variables
-load_dotenv('.env')
-
-# Initialize ElasticsearchStore
-es_vector_store = ElasticsearchStore(index_name="calls",
-                                     vector_field='conversation_vector',
-                                     text_field='conversation',
-                                     es_cloud_id=os.getenv("ELASTIC_CLOUD_ID"),
-                                     es_api_key=os.getenv("ELASTIC_API_KEY"))
-
-def get_documents_from_file(file):
-   """Reads a json file and returns list of Documents"""
-   with open(file=file, mode='rt') as f:
-       conversations_dict = json.loads(f.read())
-      
-   # Build Document objects using fields of interest.
-   documents = [Document(text=item['conversation'],
-                         metadata={"conversation_id": item['conversation_id']})
-                for
-                item in conversations_dict]
-   return documents
-
-def run_ingestion_pipeline():
-    # Embedding Model to do local embedding using Ollama.
-    ollama_embedding = OllamaEmbedding("mistral")
-
-    # LlamaIndex Pipeline configured to take care of chunking, embedding
-    # and storing the embeddings in the vector store.
-    pipeline = IngestionPipeline(
-        transformations=[
-            SentenceSplitter(chunk_size=350, chunk_overlap=50),
-            ollama_embedding,
-        ],
-        vector_store=es_vector_store
-    )
-
-    # Load data from a json file into a list of LlamaIndex Documents
-    documents = get_documents_from_file(file="conversations.json")
-
-    pipeline.run(documents=documents)
-    print(".....Done running pipeline.....\n")
-
-@app.post("/ingest")
-def run_pipeline():
+@app.post("/process-query")
+def process_query(query: str):
     try:
-        run_ingestion_pipeline()
-        return {"message": "Data ingested successfully"}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
-class QueryRequest(BaseModel):
-    query: str
-
-@app.post("/query")
-def process_query(query_request: QueryRequest):
-    try:
-        result = run_query(query_request.query)
+        result = run_query(query)
         return {"result": result}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
--- query.py
+++ query.py
@@ -2,9 +2,9 @@
 from llama_index.core import VectorStoreIndex, QueryBundle, Response, Settings
 from llama_index.embeddings.ollama import OllamaEmbedding
 from llama_index.llms.ollama import Ollama
-from main import es_vector_store
+from index import es_vector_store
 
-def run_query(query: str):
+def run_query(query: str) -> Response:
     # Local LLM to send user query to
     local_llm = Ollama(model="mistral")
     Settings.embed_model = OllamaEmbedding("mistral")
@@ -12,11 +12,9 @@ def run_query(query: str):
     index = VectorStoreIndex.from_vector_store(es_vector_store)
     query_engine = index.as_query_engine(local_llm, similarity_top_k=10)
 
+    # Create a QueryBundle with the provided query
     bundle = QueryBundle(query, embedding=Settings.embed_model.get_query_embedding(query))
+
+    # Query the vector store index
     result = query_engine.query(bundle)
     return result
-
-# Usage example:
-query = "Give me summary of water related issues"
-result = run_query(query)
-print(result)
